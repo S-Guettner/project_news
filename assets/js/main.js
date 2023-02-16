9db4e7b0e961fc9d10ac2d2edd3b273cd75ae1f7
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-16 15:01:51 +0100

    styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 const APIKEY = "4ee3f9f62d1144ca9677754d4b9bb0f2"
+const topicInput = document.querySelector("#topicInput")
+
+
 let searchTerm = "tesla"
+
+
+
+
 
 fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&from=2023-01-16&sortBy=publishedAt&apiKey=${APIKEY}`)
     .then(response => response.json())
